b9614583e59a5cef57207eadfa6aaf889edf0259
Dave Footitt <dave@bogus.net>
2014-06-20 15:48:45 +0100

    Sample xml added and begin xml parsing
    Requires TinyXml

--- Compiler.cpp
+++ Compiler.cpp
@@ -1,17 +1,58 @@
 #include "stdafx.h"
 #include "Compiler.h"
 
-
 Compiler::Compiler(void)
 {
 }
-
 
 Compiler::~Compiler(void)
 {
 }
 
+void Compiler::parseMessages( TiXmlElement* rootElement )
+{
+    auto messageElement = rootElement->FirstChildElement("messages");
+
+    if (messageElement)
+    {
+        for (auto i=messageElement->FirstChildElement("msg"); i!=nullptr; i=i->NextSiblingElement())
+        {
+            if (i->Attribute("id")!=nullptr)
+                m_messages.insert(std::make_pair(i->Attribute("id"),i->GetText()));
+        }
+    }
+}
+
+void Compiler::parseRooms( TiXmlElement* )
+{
+
+}
+
 bool Compiler::compileFile( const std::string& sourceFile )
 {
-    return true;
+    TiXmlDocument doc(sourceFile);
+
+    if (doc.LoadFile())
+    {
+        TiXmlElement* rootElement = doc.RootElement();
+        reset();
+
+        parseMessages(rootElement);
+        //parseVerbs();
+        //parseNouns();
+        //parseAdverbs();
+        //parseObjects();
+        //parseHighConditions();
+        //parseLowConditions();
+        //parseRooms(rootElement);
+
+        return true;
+    }
+
+    return false;
+}
+
+void Compiler::reset()
+{
+    m_messages.clear();
 }
--- Compiler.h
+++ Compiler.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <map>
+#include "tinyxml.h"
 
 class Compiler
 {
@@ -10,4 +12,13 @@ public:
 
 public:
     bool compileFile(const std::string& sourceFile);
+
+protected:
+    void reset();
+    void parseMessages(TiXmlElement*);
+    void parseRooms(TiXmlElement*);
+
+    typedef std::string idType;
+
+    std::map<idType,std::string> m_messages;
 };
